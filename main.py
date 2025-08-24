3c2ee0693c16603cd575618715a0ae0a5171048e
Vaishnavi Kute <kutevaishnavi9@gmail.com>
2025-08-24 23:43:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def calculatorDisplay():
+    display = """ Welcome to Calculator
+
+Choose one operation:
+1. Add
+2. Subtract
+3. Exit
+"""
+
+
+    return(display)
+    
+if __name__ == '__main__':
+    print(calculatorDisplay())
